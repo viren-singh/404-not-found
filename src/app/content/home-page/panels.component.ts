fb5715363b4d608bc5604ea0be221c9d2b4aa769
Neharkar, Atul (US - Mumbai) <aneharkar@deloitte.com>
2017-09-08 16:36:12 +0530

    Adding styles

--- src/app/content/home-page/panels.component.ts
+++ src/app/content/home-page/panels.component.ts
@@ -6,9 +6,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PanelsComponent implements OnInit {
 
+	public panelist;
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  panelists = [
+  	{
+  		img: "",
+  		name: "Lorem ipsum",
+  		title: "FE Developer"
+  	},
+  	{
+  		img: "",
+  		name: "Lorem ipsum",
+  		title: "Visual Designer"
+  	},
+  	{
+  		img: "",
+  		name: "Lorem ipsum",
+  		title: "Visual Designer"
+  	},
+  	{
+  		img: "",
+  		name: "Lorem ipsum",
+  		title: "FE Developer"
+  	}
+  ];
 
 }
