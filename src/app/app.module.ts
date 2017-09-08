9168e7f499cb24ee6558063153a8c9bc1c0eb47d
Neharkar, Atul (US - Mumbai) <aneharkar@deloitte.com>
2017-09-08 14:44:08 +0530

    Adding header and banner structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,31 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContentComponent } from './content/content.component';
+import { HomePageComponent } from './content/home-page/home-page.component';
+import { BannerComponent } from './content/home-page/banner.component';
+import { JourniesComponent } from './content/home-page/journies.component';
+import { PanelsComponent } from './content/home-page/panels.component';
+import { AboutUsComponent } from './content/about-us/about-us.component';
+import { ElementsComponent } from './content/elements/elements.component';
+import { FaqComponent } from './content/faq/faq.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ContentComponent
+    ContentComponent,
+    HomePageComponent,
+    BannerComponent,
+    JourniesComponent,
+    PanelsComponent,
+    AboutUsComponent,
+    ElementsComponent,
+    FaqComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
